7433fe216ce312cff817a135f9f64c5f72564711
Justin Liu <44413809+JustACowSP@users.noreply.github.com>
2022-06-14 13:20:21 -0400

    Add function to play a single round

--- script.js
+++ script.js
@@ -2,3 +2,21 @@ function computerPlay() {
     moves = ['Rock', 'Paper', 'Scissors']
     return moves[Math.floor(Math.random() * moves.length)]
 }
+
+function capitalize(str) {
+    return str[0].toUpperCase() + str.substr(1).toLowerCase()
+}
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = capitalize(playerSelection)
+
+    if (playerSelection == computerSelection) {
+        return `Tie! You both played ${playerSelection}.`
+    } else if (playerSelection == 'Rock' && computerSelection == 'Scissors' ||
+        playerSelection == 'Paper' && computerSelection == 'Rock' ||
+        playerSelection == 'Scissors' && computerSelection == 'Rock') {
+        return `You win! ${playerSelection} beats ${computerSelection}.`
+    } else {
+        return `You lose! ${computerSelection} beats ${playerSelection}.`
+    }
+}
